de102d30c6960df3ad31e7a86ae4a9002c321525
Ajeesh Damodaran <ajeesh.damodaran@vizientinc.com>
2018-10-29 13:00:20 -0500

    Updated

--- DockerApi/Startup.cs
+++ DockerApi/Startup.cs
@@ -6,7 +6,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-
+using System.Collections.Generic;
+using System.Data.SqlClient;
 namespace DockerApi
 {
     public class Startup
@@ -31,12 +32,14 @@ namespace DockerApi
                        .AllowAnyHeader();
             }));
 
-           
+
             var hostname = Environment.GetEnvironmentVariable("SQLSERVER_HOST") ?? "localhost";
             var password = Environment.GetEnvironmentVariable("SA_PASSWORD") ?? "Ajeesh250282#";
-            var connString = $"Server={hostname};Database=DockerTestDb;User=sa;Password={password};";
+            // var connString = $"Server={hostname};Database=DockerTestDb;User=sa;Password={password};";
+            var connString = Configuration.GetConnectionString("DefaultConnection");
             //SqlConnection con = new SqlConnection(connString);
             //con.Open(); 
+
             services.AddDbContext<ApiContext>(options => options.UseSqlServer(connString));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -49,7 +52,14 @@ namespace DockerApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
+                //var cnx = new SqlConnection(Configuration.GetConnectionString("DefaultConnection"));
+                //var evolve = new Evolve.Evolve(cnx)
+                //{
+                //    Locations = new List<string> { @"flyway\sql" },
+                //    IsEraseDisabled = true,
+                //    Driver= "SqlClient",
+                //};
+                //evolve.Migrate();
             }
 
             app.UseCors("MyPolicy");
---
+++ DockerApi/flyway/sql/V001__Create_Title_Table.sql
@@ -0,0 +1,19 @@
+
+USE [DockerTestDb];
+GO
+
+CREATE TABLE dbo.Products (
+    [Id] int NOT NULL IDENTITY,
+    [Name] varchar(500) NOT NULL,
+	[Price] Decimal(18,2) NOT NULL,
+    CONSTRAINT [PK_ProductId] PRIMARY KEY ([Id])
+);
+GO
+
+
+
+INSERT INTO dbo.Products (Name,Price) VALUES ('Laptop', 1000.00)
+INSERT INTO dbo.Products (Name,Price) VALUES ('Mouse', 10.00)
+INSERT INTO dbo.Products (Name,Price) VALUES ('Monitor', 200.00)
+
+GO
